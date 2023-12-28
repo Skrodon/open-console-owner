ee3184ea33d8d1bf20c7dd9bfd2017f0ffd974ca
NguyenTaiLoc <nguyentailoc@NguyenTaiLocs-MacBook-Pro.local>
2023-12-28 17:08:04 +0700

    solve conflict

--- lib/OwnerConsole/Controller/Login.pm
+++ lib/OwnerConsole/Controller/Login.pm
@@ -1,69 +1,49 @@
 package OwnerConsole::Controller::Login;
 use Mojo::Base 'Mojolicious::Controller';
 
+use Crypt::PBKDF2 ();
+my $crypt = Crypt::PBKDF2->new;
+
+#XXX $self->users->collection('users') will be move to OpenConsole::Model::Users
+#XXX when this works.
+
 use constant {
 	EXPIRE_SESSION => 600,  # seconds of inactivity
 };
 
-sub index($)
-{	my ($self, $error) = @_;
-	$self->notify(error => $error) if defined $error;
+###### Login
+
+sub index()
+{	my $self = shift;
 
 	if($self->session('is_auth'))
-	{	$self->frontpage;
+	{	$self->redirect_to('/dashboard');
 	}
 	else
 	{	$self->render(template => 'login/index');
 	}
 }
 
-sub logout()
+sub tryLogin()
 {	my $self = shift;
-	$self->session(expires => 1);  # Kill the Session
-	$self->render(template => 'login/logout');
-}
-
-sub register()
-{	my $self = shift;
-
-	my $user     = uc $self->param('username');
-	my $password = $self->param('password');
-	my $confirm  = $self->param('confirm_password');
-
-	if($password ne $confirm)
-	{	$self->notify(error => "The passwords are not the same.");
-		$self->render(template => 'login/register');
-	}
-}
-
-sub isValidUser
-{	my $self = shift;
-
-	# List of registered users
-	my %validUsers = ( "JANE" => "welcome123"
-					  ,"JILL" => "welcome234"
-					  ,"TOM"  => "welcome345"
-					  ,"RAJ"  => "test123"
-					  ,"RAM"  => "digitalocean123"
-					 );
-
-	my $user     = uc $self->param('username') =~ s/\@.*//r;
+	my $email    = $self->param('username');
 	my $password = $self->param('password');
 
 	# First check if the user exists
-	if($validUsers{$user})
+	my $user = $self->users->collection('users')->find_one({user => lc $email});
+	unless(defined $user)
 	{	$self->notify(error => "You are not a registered user");
 		return $self->index;
 	}
 
 	# Validating the password of the registered user
-	if($validUsers{$user} ne $password)
+	unless($crypt->verify($user->{password}, $password))
 	{	$self->notify(error => "Invalid password, please try again");
-		$self->index;
+		return $self->index;
 	}
 
 	$self->login($user);  # Creating session cookies
-	$self->redirect_to('/dashboard');       # Re-direct to home page
+	$self->redirect_to('/dashboard');     # Re-direct to home page
 }
 
 sub login($)
@@ -75,10 +55,43 @@ sub login($)
 
 sub mustBeLoggedIn($)
 {	my $self = shift;
+warn "IS AUTH?";
 	return 1 if $self->session('is_auth');
+warn "NO";
 
 	$self->notify(error => "You are not logged in, please login to access this.");
 	$self->index;
+}
+
+###### Logout
+
+sub logout()
+{	my $self = shift;
+	$self->session(expires => 1);  # Kill the Session
+	$self->render(template => 'login/logout');
+}
+
+###### Register
+
+sub register()
+{	my $self = shift;
+	$self->render(template => 'login/register');
+}
+
+sub tryRegister()
+{	my $self = shift;
+	my $email    = $self->param('username');
+	my $password = $self->param('password');
+	#XXX use Email::Valid to check email, otherwise return with notify(error)
+	#XXX check password length
+
+	my $encr_passwd = $crypt->generate($password);
+	$self->users->collection('users')
+		->insert({ user => lc $email, email => $email, password => $encr_passwd });
+
+	$self->notify(warning => 'User is created successfully');
+	$self->login(lc $email);
+	$self->redirect_to('/dashboard');
 }
 
 1;
