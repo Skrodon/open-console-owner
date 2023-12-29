fcc3c1b8aa6890571139587de0737a014eaea78a
Mark Overmeer <mark@overmeer.net>
2023-12-29 15:35:25 +0100

    userdb week

--- lib/OwnerConsole/Model/Users.pm
+++ lib/OwnerConsole/Model/Users.pm
@@ -2,6 +2,7 @@ package OwnerConsole::Model::Users;
 use Mojo::Base -base;
 
 use OwnerConsole::User ();
+use OwnerConsole::Identity ();
 
 =section DESCRIPTION
 This object handles the "users" database, which contains all information
@@ -9,14 +10,13 @@ which related to perticular people with a login.
 
 collections:
 =over 4
-=item * 'user': the login of a user, including a change-log
+=item * 'user': the login of a user
+=item * 'identity': the public representation of a Person
 =back
 
 =cut
 
-has 'db';
-
-use Data::Dumper;
+has db => undef, weak => 1;
 
 sub createUser($%)
 {	my ($self, $user, %args) = @_;
@@ -30,9 +30,10 @@ sub createUser($%)
 
 sub user($)
 {	my ($self, $user) = @_;
-	my $data = $self->db->collection('user')->find_one() # ({user => $user})
+	my $data = $self->db->collection('user')->find_one({user => $user})
 		or return;
 
+use Data::Dumper;
 warn "getUser: ", Dumper $data;
 	OwnerConsole::User->fromDB($data);
 }
