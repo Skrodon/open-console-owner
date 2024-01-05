feb42965bb64a344400345e5ab5ac47a86a61c4c
Mark Overmeer <mark@overmeer.net>
2024-01-05 20:10:41 +0100

    make account delete button work again.

--- public/assets/login.js
+++ public/assets/login.js
@@ -55,21 +55,6 @@ function activate_account_settings(form) {
 	const originalEmail = emailInput.val();
 	const deleteModal   = $('#delete_account_confirmation_modal', form);
 
-	emailInput.on('input', function () {
-		const isEmailChanged = emailInput.val() !== originalEmail;
-		changeEmailButton.toggleClass('changed', isEmailChanged);
-	});
-
-	$('#change_email_button', form).on('click', function (event) {
-		if(changeEmailButton.hasClass('changed')) {
-			// prevent page to reload when form clicked
-			event.preventDefault();
-		
-			// Must make AJAX to call server API later
-			alert('Change email button clicked');
-		}
-	});
-
 	$('#delete_account_button', form).on('click', function(event)  {
 		event.preventDefault();
 		deleteModal.show();
@@ -82,7 +67,7 @@ function activate_account_settings(form) {
 	$('#confirm_delete_button', form).on('click', function () {
 		deleteModal.hide();
 // form.submit()
-// in handler:  $self->notify(''), redirect to /
+// in handler:  $self->notify(''), redirect to frontpage
 		alert('Account deleted successfully');
 	});
 }
@@ -90,6 +75,6 @@ function activate_account_settings(form) {
 $(document).ready(function() {
 	$("form#register").map(function () { activate_register($(this)) });
 	$("form#login").map(function () { activate_login($(this)) });
-	$("form#change_account").map(function () { activate_account_settings($(this)) });
+	$("form#config_account").map(function () { activate_account_settings($(this)) });
 })
 
