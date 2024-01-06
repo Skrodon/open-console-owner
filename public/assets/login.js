52f97b0bb25074a51efe3f79ab334142d075f184
NguyenTaiLoc <nguyentailoc@NguyenTaiLocs-MacBook-Pro.local>
2024-01-06 12:13:18 +0700

    register page with input-group

--- public/assets/login.js
+++ public/assets/login.js
@@ -1,45 +1,46 @@
 function togglePasswordField(field, toggleButton) {
-    var type = field.attr('type') === 'password' ? 'text' : 'password';
-    field.attr('type', type);
-    toggleButton.removeClass().addClass(type === 'password'
-      ? 'fa-solid fa-eye eye_toggle'
-      : 'fa-solid fa-eye-slash eye_toggle');
-  }
-
-  function activate_login(form) {
-    var loginPassword = $('#password', form);
-    $('#toggle_login_password', form).on('click', function() {
-      togglePasswordField(loginPassword, $(this));
-    });
-  }
-
-function validatePassword(form, passwordField, confirmField)
-{	var password = passwordField.val();
-	var confirmPassword = confirmField.val();
-
-	$(".warning", form).hide();
-
-	if(password.length < 6) {
-		$('#password_length_warning', form).show();
-		return false;
-	}
-	if(password !== confirmPassword) {
-		$('#password_match_warning', form).show();
-		return false;
-	}
-
-	return true;
+	var type = field.attr('type') === 'password' ? 'text' : 'password';
+	field.attr('type', type);
+	toggleButton.removeClass().addClass(type === 'password'
+		? 'fa-solid fa-eye eye_toggle'
+		: 'fa-solid fa-eye-slash eye_toggle');
 }
+
+function activate_login(form) {
+	var loginPassword = $('#password', form);
+	$('#toggle_login_password', form).on('click', function() {
+		togglePasswordField(loginPassword, $(this));
+	});
+}
+
+function validatePassword(form, passwordField, confirmField) {
+    var password = passwordField.val();
+    var confirmPassword = confirmField.val();
+
+    $(".warning", form).hide();
+
+    if (password.length < 6) {
+        $('#password_length_warning', form).show();
+        return false;
+    }
+    if (password !== confirmPassword) {
+        $('#password_match_warning', form).show();
+        return false;
+    }
+
+    return true;
+}
+
 
 function activate_register(form)
 {	var passwordField = $('#password', form);
 	var confirmField  = $('#confirm', form);
 
-	$('#togglePassword', form).on('click', function() {
+	$('#toggle_password', form).on('click', function() {
 		togglePasswordField(passwordField, $(this));
 	});
 
-	$('#toggleConfirmPassword', form).on('click', function() {
+	$('#toggle_confirm_password', form).on('click', function() {
 		togglePasswordField(confirmField, $(this));
 	});
 
