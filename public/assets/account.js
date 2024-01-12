df195585522a93ed79d20187f4506b174ce4c54d
NguyenTaiLoc <nguyentailoc@NguyenTaiLocs-MacBook-Pro.local>
2024-01-12 17:13:01 +0700

    add language to table when select languages #13

--- public/assets/account.js
+++ public/assets/account.js
@@ -2,20 +2,20 @@ function activate_language_selector(form) {
 	var table = $("#langtab", form).first();
     var uniqueLangs = {};
 
-	$('.remove-link', table).on('click', function (event) {
-		event.preventDefault();
-		$(this).closest('tr').remove();
-	});
+	table.on('click', '.remove-link', function (event) {
+        event.preventDefault();
+        $(this).closest('tr').remove();
+    });
 
 	// Helper make row not collapse when sort
 	function fixHelperModified(event, tr) {
-		var $originals = tr.children();
-		var $helper = tr.clone();
-		$helper.children().each(function(index)
+		var originals = tr.children();
+		var helper = tr.clone();
+		helper.children().each(function(index)
 		{
-		  $(this).width($originals.eq(index).width())
+		  $(this).width(originals.eq(index).width())
 		});
-		return $helper;
+		return helper;
 	};
 
 	$("#langtab tbody").sortable({
@@ -23,14 +23,46 @@ function activate_language_selector(form) {
 	}).disableSelection();
 
 	// Remove duplicate language
-    $('#langtab tbody tr').each(function () {
-      var langName = $(this).find('td:first-child').text().trim();
-      if (uniqueLangs[langName]) {
-        $(this).remove();
-      } else {
-        uniqueLangs[langName] = true;
-      }
-    });
+	$('#langtab tbody tr').each(function () {
+		var langName = $(this).find('td:first-child').text().trim();
+		if (uniqueLangs[langName]) {
+		  $(this).remove();
+		} else {
+		  uniqueLangs[langName] = true;
+		}
+	});
+
+	// Loop through langtab checks if language existed when adds new language
+    function isLanguageExists(language) {
+		return $('#langtab td:first-child').toArray().some(function (element) {
+			return $(element).text() === language;
+		});
+	}
+	
+	$('#languages').change(function () {
+		var selectedLanguages = $(this).find('option:selected').map(function() {
+			return $(this).text();
+		}).get();
+
+	
+		if (selectedLanguages.length > 0) {
+			$.each(selectedLanguages, function(index, selectedLanguage) {
+				if (!isLanguageExists(selectedLanguage)) {
+					var newRow = '<tr>' +
+						'<td class="text-center align-middle">' + selectedLanguage + '</td>' +
+						'<td class="text-center"> \
+							<a href="#" class="btn btn-danger remove-link" title="Remove"> \
+								<i class="fa fa-times" aria-hidden="true"></i> \
+							</a> \
+						</td>' +
+						'</tr>';
+					$('#langtab').append(newRow);
+
+					$('#languages option:contains("' + selectedLanguage + '")').prop('selected', false);	
+				}
+			});
+		}
+	});
 }
 
 function activate_timezone_selector() {
