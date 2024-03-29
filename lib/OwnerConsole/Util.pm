82bd36c7381cb80e991215553a664f00c6a9431c
Mark Overmeer <mark@overmeer.net>
2024-03-29 12:29:59 +0100

    add some docs to Util

--- lib/OwnerConsole/Util.pm
+++ lib/OwnerConsole/Util.pm
@@ -38,7 +38,22 @@ our %EXPORT_TAGS = (
    validate => [ @is_valid, @validators ],
 );
 
+=chapter NAME
+
+OwnerConsole::Util - collection of useful functions
+
+=chapter FUNCTIONS
+=section Practical
+
+=function flat @anything
+Flatten ARRAYs into elements, and remove undefined elements from the list.
+=cut
+
 sub flat(@) { grep defined, map ref eq 'ARRAY' ? @$_ : $_, @_ }
+
+#----------
+=section Validation
+=cut
 
 sub val_line($)
 {	my $line = shift;
@@ -72,19 +87,39 @@ sub is_valid_url($)
 	$ !x;
 }
 
+#----------
+=section MongoDB
+=function bson2datetime $timezone
+Represent a timestamp in the MongoDB specific time format (M<Mango::BSON::Time>), in
+human readible form.
+=cut
+
 sub bson2datetime($$)
 {	my ($stamp, $tz) = @_;
 	$stamp ? DateTime->from_epoch(epoch => $stamp->to_epoch)->set_time_zone($tz) : undef;
 }
 
-# Token prefixes:
-#   A = Account
-#   C = Challenge
-#   G = Group identity
-#   I = personal Identity
-#   M = send eMail
-#   N = iNvite email
-#   P = proof
+#-----------
+=section Tokens
+Tokens are cryptographically strong unique codes.  There is no protection against the
+generation of dupplicates, because that chance is uncredably small.
+
+The unique part is preceeded by a code for the Open Console server instance, and a prefix
+which indicate its application.  The latter mainly for debugging purposes.
+
+Token prefixes:
+
+   A = Account
+   C = Challenge
+   G = Group identity
+   I = personal Identity
+   M = send eMail
+   N = iNvite email
+   P = proof
+
+=function new_token $prefix
+=function reseed_tokens
+=cut
 
 my $token_generator = Session::Token->new;
 sub new_token($)    { state $i = $::app->config->{instance}; "$i:${_[0]}:" . $token_generator->get }
